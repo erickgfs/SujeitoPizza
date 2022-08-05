d0ba0b9c1522b2ca48ea3c07dd6ecd4657876765
erickgfs <erickgfs@gmail.com>
2022-08-04 23:23:19 -0300

    tela de cadastro

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import styles from '../../styles/home.module.scss';
 
@@ -12,18 +13,20 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>SijeitoPizza - Faça seu login</title>
+        <title>SujeitoPizza - Faça seu login</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Sujeito Pizzaria"/>
 
         <div className={styles.login}>
-          <form>
-            <Input placeholder='Digite seu mail' type="text"/>
-            <Input placeholder='Sua senha' type="password"/>
-            <Button type="submit" loading={false}>Acessar</Button>
-          </form>
-          <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+            <form>
+              <Input placeholder='Digite seu email' type="text"/>
+              <Input placeholder='Sua senha' type="password"/>
+              <Button type="submit" loading={false}>Acessar</Button>
+            </form>
+            <Link href='/signup'>
+              <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+            </Link>
         </div>
       </div>
     </>
