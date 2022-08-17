04be99ce6eb501736f0752c42531d2bcd67f77c4
erickgfs <erickgfs@gmail.com>
2022-08-17 02:04:40 -0300

    Contexto/Api

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useContext, useState } from 'react'
+
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -9,7 +11,26 @@ import logoImg from '../../public/logo.svg';
 import { Input } from '../components/ui/Input';
 import { Button } from '../components/ui/Button';
 
+import { AuthContext } from '../contexts/AuthContext';
+
 export default function Home() {
+  const { signIn } = useContext(AuthContext);
+
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ loading, setLoading ] = useState(false);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email,
+      password
+    }
+
+    await signIn(data);
+  }
+
   return (
     <>
       <Head>
@@ -19,9 +40,19 @@ export default function Home() {
         <Image src={logoImg} alt="Logo Sujeito Pizzaria"/>
 
         <div className={styles.login}>
-            <form>
-              <Input placeholder='Digite seu email' type="text"/>
-              <Input placeholder='Sua senha' type="password"/>
+            <form onSubmit={handleLogin}>
+              <Input 
+                placeholder='Digite seu email' 
+                type="text" 
+                value={ email } 
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input 
+                placeholder='Sua senha' 
+                type="password" 
+                value={ password } 
+                onChange={(e) => setPassword(e.target.value)}
+              />
               <Button type="submit" loading={false}>Acessar</Button>
             </form>
             <Link href='/signup'>
