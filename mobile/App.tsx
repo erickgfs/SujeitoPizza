db71e4e5068b37bf25a9a6e60bcea7bac2158e49
erickgfs <erickgfs@gmail.com>
2022-09-14 01:08:57 -0300

    mobile inicio

---
+++ mobile/App.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, StatusBar, View } from 'react-native';
+import SignIn from './src/pages/SignIn';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar backgroundColor='#1d1d2e' barStyle="light-content" translucent={false}/>
+      <SignIn />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
