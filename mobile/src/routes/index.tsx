78e313c8e987ae4efe8fcc45152f0b0a1a3ce93f
erickgfs <erickgfs@gmail.com>
2022-09-20 00:58:51 -0300

    Tela de login

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useContext } from 'react';
+import { View, ActivityIndicator } from 'react-native';
 
 import AppRoutes from './app.routes';
 import AuthRoutes from './auth.routes';
 
+import { AuthContext } from '../contexts/AuthContext';
+
 function Routes() {
-    const isAuthenticated = false;
+    const { isAuthenticated } = useContext(AuthContext);
     const loading = false;
+
+    if(loading) {
+        return(
+            <View 
+                style={{
+                    flex:1, 
+                    backgroundColor:'#1d1d2e', 
+                    justifyContent:'center', 
+                    alignContent:'center'}
+                }>
+                <ActivityIndicator size={60} color="#f5f7fb"
+            />
+            </View>
+        )
+    }
 
     return(
         isAuthenticated ? <AppRoutes /> : <AuthRoutes />
